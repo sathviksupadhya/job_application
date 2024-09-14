69510bc05d75d39916d7b156f471372e70248ca1
sathvik s <sathviksupadhya19@gmail.com>
2024-09-14 14:00:31 +0530

    created JOB_controller and JOB_entity class

---
+++ src/main/java/com/FirstProject/Job/Application/job/Job.java
@@ -0,0 +1,67 @@
+package com.FirstProject.Job.Application.job;
+
+public class Job {
+    private Long id;
+    private String description;
+    private String title;
+    private String minSalary;
+    private String maxSalary;
+    private String location;
+
+    public Job(Long id, String description, String title, String minSalary, String maxSalary, String location) {
+        this.id = id;
+        this.description = description;
+        this.title = title;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(String minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public String getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(String maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
---
+++ src/main/java/com/FirstProject/Job/Application/job/jobController.java
@@ -0,0 +1,28 @@
+package com.FirstProject.Job.Application.job;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class jobController {
+
+
+    private List<Job> jobs = new ArrayList<>();
+
+    @GetMapping("/jobs")
+    public List<Job> findAll(){
+        return jobs;
+    }
+
+    @PostMapping("/add-jobs")
+    public String createJobs(@RequestBody Job job){
+        jobs.add(job);
+        return "job added successfuly";
+    }
+}
